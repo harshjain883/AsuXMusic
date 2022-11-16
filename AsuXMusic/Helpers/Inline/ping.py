a73202fe65679841d2cf5176083e1eed945f9102
harshjain883 <115611849+harshjain883@users.noreply.github.com>
2022-11-16 11:05:23 +0530

    Update ping.py

--- AsuXMusic/Helpers/Inline/ping.py
+++ AsuXMusic/Helpers/Inline/ping.py
@@ -11,7 +11,7 @@ ping_ig = InlineKeyboardMarkup(
                 ),
                 InlineKeyboardButton(
                     text="sᴏᴜʀᴄᴇ",
-                    url="https://github.com/Abishnoi69/AsuXMusic"
+                    url="https://te.legra.ph/file/d27c1f4c3dd09d552a0da.mp4"
                 )
             ]
         ]
